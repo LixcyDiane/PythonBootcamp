77beb45900ccbfdcc828e71f0c469adbf597bfe2
Lixcy <vega.ldv212@gmail.com>
2018-04-11 16:16:04 -0400

    Variable slicing

--- MyExample.py
+++ MyExample.py
@@ -16,3 +16,7 @@ print(variable)
 
 #variable indexing 
 print(variable[2])
+
+#variable slicing 
+print(variable[0:5:1]) #from index 0 to index 4 in steps of 1
+print(variable[0:8:2]) #from index 0 to index 7 in steps of 2
