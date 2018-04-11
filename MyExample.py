9c52d0fb3a4cd6a1186653f04ce40e634630a947
Lixcy <vega.ldv212@gmail.com>
2018-04-11 16:58:49 -0400

    Showing from specific index

--- MyExample.py
+++ MyExample.py
@@ -15,8 +15,10 @@ variable = "concentracion"
 print(variable)
 
 #variable indexing 
-print(variable[2])
+print(variable[2]) #shows character in index 2
+print(variable[4:]) #shows all characters starting from index 4
 
 #variable slicing 
 print(variable[0:5:1]) #from index 0 to index 4 in steps of 1
 print(variable[0:8:2]) #from index 0 to index 7 in steps of 2
+
