0f2e786cb5aeb663d071a7899e7d7ac2b8811dc7
Lixcy <vega.ldv212@gmail.com>
2018-04-11 16:10:19 -0400

    Variable Indexing

--- MyExample.py
+++ MyExample.py
@@ -8,7 +8,11 @@ print(a + b)
 print(type(a)) #Checks variable data type
 print(len('burrito')) #prints string length 
 
+#variables can be switched from type to type without new declaration 
 variable = 1
 print(variable)
 variable = "concentracion" 
 print(variable)
+
+#variable indexing 
+print(variable[2])
